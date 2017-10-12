a129c9bf81c185f957bd984d4c2652dd3ed4e5ae
Jawalgeri <gopinath.jawalgeri@gmail.com>
2017-10-12 21:26:39 +0530

    First Commit

---
+++ MapReduce/converting it specified format/conf.java
@@ -0,0 +1,9 @@
+import org.apache.hadoop.conf.Configuration;
+public class ConfigurationFactory {
+private ConfigurationFactory() {
+}
+private final static Configuration conf = new Configuration();
+public static Configuration getInstance() {
+return conf;
+}
+}
